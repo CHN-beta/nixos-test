5b3d74f7d8c460ad355d7f0b52faf49b6a463eb2
chn <chn@chn.moe>
2023-03-29 14:44:55 +0800

    fix p10k configuration prompt

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,7 @@
         # p10k instant prompt
         P10K_INSTANT_PROMPT="$XDG_CACHE_HOME/p10k-instant-prompt-''${(%):-%n}.zsh"
         [[ ! -r "$P10K_INSTANT_PROMPT" ]] || source "$P10K_INSTANT_PROMPT"
+        POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
       '';
 
       plugins = with pkgs; [
