36b256d334fe39ad9b6256ef46a2490918ae80c9
chn <chn@chn.moe>
2023-04-02 18:30:32 +0800

    enable qemu guest agent

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,8 @@
     pulse.enable = true;
   };
 
+  services.qemuGuest.enable = true;
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
