7037f6e6092935dc63538691d8e4513a4feaedd5
chn <chn@chn.moe>
2023-04-02 18:41:04 +0800

    fix ga

--- configuration.nix
+++ configuration.nix
@@ -77,6 +77,7 @@
   };
 
   services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
